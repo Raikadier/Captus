b848f24c32d7b4393c31fd25213d5b6ffdefcea2
Raikadier <138398472+Raikadier@users.noreply.github.com>
2025-05-13 23:18:18 -0500

    Add files via upload

---
+++ ENTITY/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+        public string Telefono { get; set; }
+        
+        public string Estado { get; set; }
+        public string Pais { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+
+    }
+}
