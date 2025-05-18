7d21fb582b211bd1e3260cc8313e95ee6c5d158e
Raikadier <138398472+Raikadier@users.noreply.github.com>
2025-05-18 15:58:02 -0500

    Add files via upload

---
+++ BLL/PriorityLogic.cs
@@ -0,0 +1,42 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+namespace BLL
+{
+    public class PriorityLogic
+    {
+        private readonly PriorityRepository priorityRepository;
+        public PriorityLogic()
+        {
+            priorityRepository = new PriorityRepository();
+        }
+        public List<Priority> GetAll()
+        {
+            try
+            {
+                return priorityRepository.GetAll();
+            }
+            catch (Exception)
+            {
+                
+                return null;
+            }
+            
+        }
+        public Priority GetById(int id)
+        {
+            try
+            {
+                return priorityRepository.GetById(id);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
