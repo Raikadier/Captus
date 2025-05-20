c373ddd9b939475e60416cf548dcf32e1457a912
Valentina Molina <valentinamolic@gmail.com>
2025-05-19 21:01:19 -0500

    Clases en capa ENTITY

---
+++ ENTITY/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public abstract class Note
+    {
+        public double ValueNote { get; set; }
+        public abstract double Calcular();
+    }
+}
